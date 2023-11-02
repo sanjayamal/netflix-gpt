5d96dd8e9f9516a0544f352e5e69ab60d7cd1ea8
sanjayamal <rajithasanjayamal0918@gmail.com>
2023-11-02 08:31:12 +0530

    firebase setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    const validateMessage = checkValidData(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(validateMessage);
   };
 
   return (
@@ -17,7 +30,10 @@ const Login = () => {
           alt="background-image"
         />
       </div>
-      <form className="absolute w-3/12 p-12 bg-black mx-auto left-0 right-0 my-36 text-white bg-opacity-70 rounded-lg">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute w-3/12 p-12 bg-black mx-auto left-0 right-0 my-36 text-white bg-opacity-70 rounded-lg"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -30,15 +46,21 @@ const Login = () => {
         )}
         <input
           type="text"
+          ref={email}
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 bg-red-600 w-full">
+        <p className="text-red-700 font-bold text-lg">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-600 w-full"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
